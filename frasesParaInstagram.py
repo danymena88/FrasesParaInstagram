029ef6288a2b2d7e4352c5d9ac86e5a28a2a5c8c
danymena88 <28245781+danymena88@users.noreply.github.com>
2023-08-26 11:11:18 -0600

    Configurar customtkinter y apscheduler

--- frasesParaInstagram.py
+++ frasesParaInstagram.py
@@ -1,8 +1,80 @@
 from process import imagenYfrase
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+import customtkinter
 
-def publicacion():
-    imagen1 = imagenYfrase("_DSC9615.png")
-    textofinal = "existe una historia que habla sobre puchita que es muy gay pero sobre todo una puchita que tiene plumas verdes y que come mucho elote y por eso le yeden los peos pero a la Menany le gusta olerlos y ellos si caen"
-    imagen1.imagen(imagen1.texto(textofinal))
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-publicacion()
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # tareas programadas
+        self.scheduler = BackgroundScheduler()
+        self.scheduler.start()
+        trigger = CronTrigger(year="*", month="*", day="*", hour="*", minute="42", second="5")
+        trigger2 = CronTrigger(year="*", month="*", day="*", hour="*", minute="54", second="5")
+        trigger3 = CronTrigger(year="*", month="*", day="*", hour="*", minute="54", second="5")
+        self.scheduler.add_job(self.publicacion,trigger=trigger,args=[],name="Tarea1",)
+        self.scheduler.add_job(self.publicacion,trigger=trigger2,args=[],name="Tarea2",)
+        self.scheduler.add_job(self.publicacion,trigger=trigger3,args=[],name="Tarea3",)
+
+        # configure window
+        self.title("CustomTkinter complex_example.py")
+        self.geometry(f"{875}x{580}")
+        self.resizable(False, False)
+
+        # configure grid layout
+        self.grid_columnconfigure((0, 1, 2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=3, padx=(0,20), sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(3, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+           
+
+        # create radiobutton frame
+        self.radiobutton_frame = customtkinter.CTkFrame(self)
+        self.radiobutton_frame.grid(row=0, column=1, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.radiobutton_frame2 = customtkinter.CTkFrame(self)
+        self.radiobutton_frame2.grid(row=0, column=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.radiobutton_frame3 = customtkinter.CTkFrame(self)
+        self.radiobutton_frame3.grid(row=0, column=3, padx=(20, 40), pady=(20, 0), sticky="nsew")
+        self.string_input_button = customtkinter.CTkButton(self.radiobutton_frame, text="Open CTkInputDialog", command=self.publicacion)
+        self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
+        self.string_input_button2 = customtkinter.CTkButton(self.radiobutton_frame2, text="Open CTkInputDialog", command=self.publicacion)
+        self.string_input_button2.grid(row=2, column=0, padx=20, pady=(10, 10))
+        self.string_input_button3 = customtkinter.CTkButton(self.radiobutton_frame3, text="Open CTkInputDialog", command=self.publicacion)
+        self.string_input_button3.grid(row=2, column=0, padx=20, pady=(10, 10))
+
+        # set default values
+        self.appearance_mode_optionemenu.set("Dark")
+        
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def publicacion(self):
+        imagen1 = imagenYfrase("_DSC9615.png")
+        imagen1.imagen(imagen1.texto(imagen1.obtenerFrase()))
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
+
+
+
+
--- process.py
+++ process.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-import requests, json
+import random, requests, json
 
 
 """ class publicacion(object):
@@ -96,7 +96,7 @@ class imagenYfrase(object):
 
   def imagen(self, text):
      W, H = (800,800)
-     texto = text
+
      # get an image
      with Image.open(self.imagen1).convert("RGBA") as base:
         # make a blank image for the text, initialized to transparent text color
@@ -106,30 +106,69 @@ class imagenYfrase(object):
         fnt = ImageFont.truetype("BebasNeue-Regular.otf", 35)
         # get a drawing context
         d = ImageDraw.Draw(txt)
+        if len(text) < 116:
+         w, h = d.textsize(text)
+         FW = (W-w*2)/1.8
+         FH = (H-h)/2.1
+        elif len(text) > 115:
+           w, h = d.textsize(text)
+           FW = (W-w*2)/1.8
+           FH = (H-h)/2.3
+        elif len(text) > 300:
+           w, h = d.textsize(text)
+           FW = (W-w*2)/1.8
+           FH = (H-h)/2.4
 
         # draw text, full opacity
-        w, h = d.textsize(texto)
-        d.text(((W-w*2)/2,(H-h)/2), texto, font=fnt, fill=(255, 255, 255, 255))
+        w, h = d.textsize(text)
+        d.text((FW,FH), text, font=fnt, fill=(255, 255, 255, 255))
 
         out = Image.alpha_composite(base, txt)
-        out.show()
+        
+        out2 = out.convert('RGB')
+        out2.save("frase.jpg")
+        #out.save("img1.png","PNG")
 
 
   def texto(self, text):
-     if len(text) > 52:
-        for i in range(52, 0, -1):
+     if len(text) > 53:
+        for i in range(53, 0, -1):
            if text[i] == " ":
                  if (53 - len(text[0:i])) > 1:
                      n = (53 - int(len(text[0:i])))
-                     resultado = (int(n * 0.8) * " ") + text[0:i] + "\n"
+                     resultado = ((int(n * 0.5)) * " ") + text[0:i] + "\n"
                      text = text[i+1:len(text)]
                      break
+                 elif (53 - len(text[0:i])) > 3:
+                     print(2)
+                     n = (53 - int(len(text[0:i])))
+                     resultado = ((int(n * 1.5)) * " ") + text[0:i] + "\n"
+                     text = text[i+1:len(text)]
+                     break
+                 else:
+                     n = (53 - int(len(text[0:i])))
+                     resultado = text[0:i] + "\n "
+                     text = text[i+1:len(text)]
+                     break                     
         return resultado + self.texto(text)
      else:
-        if (52 - len(text)) > 1:
-                 n = int(52 - len(text))
-                 text = (int(n*0.8) * " ") + text
+        if (52 - len(text)) < 7:
+                 n = int(53 - len(text))
+                 text = ((int(n * 1)) * " ") + text
                  return text
+        elif (52 - len(text)) > 6:
+                 n = int(53 - len(text))
+                 text = ((int(n * 1)) * " ") + text
+                 return text
+        
+  def obtenerFrase(self):
+      with open('frases.json', encoding='utf-8') as openfile:
+                # Reading from json file
+                frase = json.load(openfile)
+      numero = random.randrange(0, len(frase), 1)
+      return frase[numero]
+      
+        
            
               
         
